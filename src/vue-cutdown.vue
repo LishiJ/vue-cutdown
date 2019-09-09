879919f905de89a312a8d076a159629223807670
李世杰 <lisj@mxchip.com>
2019-09-09 18:29:30 +0800

    feat: Static template

--- src/vue-cutdown.vue
+++ src/vue-cutdown.vue
@@ -1,15 +1,59 @@
 <template>
-  <div class="vue-cutdown">
-    Hello VueCutdown
-  </div>
+  <p class="vue-cutdown">
+    <span v-if="cutdown.day > 0"
+      ><span>{{ cutdown.day }}</span
+      ><i>{{ dayTxt }}</i></span
+    >
+    <span>{{ cutdown.hour }}</span>
+    <i>{{ hourTxt }}</i>
+    <span>{{ cutdown.minutes }}</span>
+    <i>{{ minutesTxt }}</i>
+    <span>{{ cutdown.seconds }}</span>
+  </p>
 </template>
 
 <script>
 export default {
   name: 'VueCutdown',
-  props: {},
+  props: {
+    /**
+     * Cutdown day_text
+     */
+    dayTxt: {
+      type: String,
+      default: '天'
+    },
+    /**
+     * Cutdown hour_text
+     */
+    hourTxt: {
+      type: String,
+      default: ':'
+    },
+    /**
+     * Cutdown minutes_text
+     */
+    minutesTxt: {
+      type: String,
+      default: ':'
+    },
+    /**
+     * Cutdown seconds_text
+     */
+    secondsTxt: {
+      type: String,
+      default: ':'
+    }
+  },
   data() {
-    return {}
+    return {
+      cutdown: {
+        day: 0,
+        hour: 1,
+        minutes: 10,
+        seconds: 20
+      }
+    }
   }
 }
 </script>
